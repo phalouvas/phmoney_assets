414022c06fcff029e8e2debad6f98ac2f7d908e0
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-05-21 08:34:31 +0300

    use moment v2.24

--- dist/assets/TransactionsCreate.6dafa9c8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./TransactionsEdit.0ff82d0f.js";import{d as a,r as t,o as s,c as n,w as r,a as c,b as m}from"./main.faceb598.js";const _={class:"bg-white shadow mt-4 prose max-w-none"},u=a({name:"TransactionsCreate",setup(i){return(p,d)=>{const e=t("FormLayout");return s(),n(e,{title:"Create Transaction"},{default:r(()=>[c("div",_,[m(o)])]),_:1})}}});export{u as default};
