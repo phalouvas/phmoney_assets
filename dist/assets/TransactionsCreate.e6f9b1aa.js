b4d9a852622b6fe8b663e153c3c82bf6351a2cb6
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-07-22 05:55:04 +0000

    changed reports index appearance

--- dist/assets/TransactionsCreate.e6f9b1aa.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./TransactionsEdit.1c0e9ba9.js";import{d as a,r as t,o as s,c as n,w as r,a as c,b as m}from"./main.d1319642.js";const _={class:"bg-white shadow mt-4 prose max-w-none"},u=a({name:"TransactionsCreate",setup(i){return(p,d)=>{const e=t("FormLayout");return s(),n(e,{title:"Create Transaction"},{default:r(()=>[c("div",_,[m(o)])]),_:1})}}});export{u as default};
--- src/components/reports/ReportsIndex.vue
+++ src/components/reports/ReportsIndex.vue
@@ -1,189 +1,126 @@
 <template>
   <div class="p-6">
-    <div class="grid grid-cols-2 prose max-w-none border-b">
-      <h3></h3>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'reports.transactions' }">
-            Transactions Report</RouterLink
-          >
-        </li>
-      </ul>
-    </div>
+    <table class="table-fixed">
+      <tbody>
+        <tr>
+          <td></td>
+          <td>
+            <RouterLink :to="{ name: 'reports.transactions' }">
+              Transactions Report</RouterLink>
+          </td>
+        </tr>
+        <tr>
+          <th colspan="2">Assets & Liabilities</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <RouterLink :to="{ name: 'reports.assets_liabilities.assets_columnchart' }">Assets Column Chart
+            </RouterLink>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.assets_piechart' }">Assets Pie Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }">Balance Sheet</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.general_ledger' }">General Ledger</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.liabilities_columnchart' }">Liabilities Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.liabilities_piechart' }">Liabilities Piechart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.networth_columnchart' }">Net Worth Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.assets_liabilities.networth_linechart' }">Net Worth Linechart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <th colspan="2">Business</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.business.customer_report' }">Customer Report</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.business.customer_summary' }">Customer Summary</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.business.employee_report' }">Employee Report</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.business.vendor_report' }">Vendor Report</RouterLink></td>
+        </tr>
+        <tr>
+          <th colspan="2">Income & Expense</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.cash_flow' }">Cash Flow</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.cash_flow_columnchart' }">Cash Flow Column Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.expenses_columnchart' }">Expenses Column Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.expenses_piechart' }">Expenses Piechart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.incomeexpense_columnchart' }">Income & Expense Column Chart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.incomeexpense_linechart' }">Income & Expense Linechart
+          </RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.income_columnchart' }">Income Column Chart</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.income_piechart' }">Income Piechart</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.profit_loss' }">Profit & Loss</RouterLink></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><RouterLink :to="{ name: 'reports.income_expense.trial_balance' }">Trial Balance</RouterLink></td>
+        </tr>
+      </tbody>
+    </table>
 
-    <div class="grid grid-cols-2 prose max-w-none border-b">
-      <h3>Assets & Liabilities</h3>
-      <ul>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.assets_columnchart' }"
-            >Assets Column Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.assets_piechart' }"
-            >Assets Pie Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Balance Forecast (pending)
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Balance Sheet</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >General Journal (pending)
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.general_ledger' }"
-            >General Ledger</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Investment Portfolio (pending)</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.liabilities_columnchart' }"
-            >Liabilities Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.liabilities_piechart' }"
-            >Liabilities Piechart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.networth_columnchart' }"
-            >Net Worth Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.assets_liabilities.networth_linechart' }"
-            >Net Worth Linechart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Price Scatterplot (pending)
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Security Piechart (pending)
-          </RouterLink>
-        </li>
-      </ul>
-    </div>
-
-    <div class="grid grid-cols-2 prose max-w-none border-b">
-      <h3>Business (pending)</h3>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'reports.business.customer_report' }"
-            >Customer Report</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.business.customer_summary' }"
-            >Customer Summary</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.business.employee_report' }"
-            >Employee Report</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.business.vendor_report' }"
-            >Vendor Report</RouterLink
-          >
-        </li>
-      </ul>
-    </div>
-
-    <div class="grid grid-cols-2 prose max-w-none border-b">
-      <h3>Income & Expense</h3>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'reports.income_expense.cash_flow' }"
-            >Cash Flow</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.income_expense.cash_flow_columnchart' }"
-            >Cash Flow Column Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Equity Statement (pending)
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.income_expense.expenses_columnchart' }"
-            >Expenses Column Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.income_expense.expenses_piechart' }"
-            >Expenses Piechart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.income_expense.incomeexpense_columnchart' }"
-            >Income & Expense Column Chart
-          </RouterLink>
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.income_expense.incomeexpense_linechart' }"
-            >Income & Expense Linechart</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink
-            :to="{ name: 'reports.income_expense.income_columnchart' }"
-            >Income Column Chart</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.income_expense.income_piechart' }"
-            >Income Piechart</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.assets_liabilities.balance_sheet' }"
-            >Income and GST Statement (pending)</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.income_expense.profit_loss' }"
-            >Profit & Loss</RouterLink
-          >
-        </li>
-        <li>
-          <RouterLink :to="{ name: 'reports.income_expense.trial_balance' }"
-            >Trial Balance</RouterLink
-          >
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
